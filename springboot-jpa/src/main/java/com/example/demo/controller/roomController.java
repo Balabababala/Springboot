f0da2f9428d1772a482aaa80f0bcd76746b61998
Balabababala <139853702+Balabababala@users.noreply.github.com>
2025-05-19 16:53:52 +0800

    05191653

--- springboot-jpa/src/main/java/com/example/demo/controller/roomController.java
+++ springboot-jpa/src/main/java/com/example/demo/controller/roomController.java
@@ -5,14 +5,23 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.dto.RoomDto;
 import com.example.demo.service.RoomService;
 
+import jakarta.validation.Valid;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,8 +32,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * GET 				/room/{roomId}			查詢指定會議室(單筆)
  * GET				/rooms					查詢所有會議室(多筆)
  * POST				/room					新增會議室
- * POST				/room/update/{roomId}	完整修改會議室(同時修改 roomName 與 roomSize)
- * GET				/room/delete/{roomId}   刪除會議室
+ * PUT				/room/update/{roomId}	完整修改會議室(同時修改 roomName 與 roomSize)
+ * DELETE			/room/delete/{roomId}   刪除會議室
  *
  */
 
@@ -35,25 +44,58 @@ public class roomController {
 	@Autowired
 	private RoomService roomService;
 	
+	@GetMapping("/{roomId}")
+	public String findByid(@PathVariable Integer roomId,@ModelAttribute RoomDto roomDto,Model model) {
+		roomDto = roomService.getRoomById(roomId);
+		return "/room/room_update";
+	}
+	
+	
 	@GetMapping
-	public String getRooms(Model model) {
-		RoomDto roomDto = new RoomDto();
+	public String getRooms(@ModelAttribute RoomDto roomDto,Model model) {
+		
+//		RoomDto roomDto = new RoomDto();
+//		model.addAttribute("roomDto",roomDto);		
+		
 		List <RoomDto> roomDtos= roomService.findAllRoom();
-		model.addAttribute("roomDto",roomDto);
 		model.addAttribute("roomDtos",roomDtos);
 		return "room/room";
 	}
 	
-	@GetMapping("/{roomId}")
-	public String findByid(@RequestParam Integer roomId,Model model) {
-		return new String();
+	
+	@PostMapping("")
+	public String addRoom(@Valid RoomDto roomDto,BindingResult bindingResult,Model model) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("roomDtos",roomService.findAllRoom());
+			return "room/room";
+		}
+		roomService.addRoom(roomDto);
+		return "redirect:/rooms";
 	}
 	
-	@PostMapping("path")
-	public String postMethodName(@RequestBody String entity) {
-		//TODO: process POST request
+	
+	@PutMapping("/update/{roomId}")
+	public String updateRoom(@PathVariable Integer roomId, @Valid RoomDto roomDto, BindingResult bindingResult) {
+		// 驗證資料
+		if(bindingResult.hasErrors()) { // 若驗證時有錯誤發生
+			return "room/room_update";
+		}
 		
-		return entity;
+		// 進行修改
+		roomService.updateRoom(roomId, roomDto);
+		return "redirect:/rooms";
 	}
 	
+	@DeleteMapping("/delete/{roomId}")
+	public String updateRoom(@PathVariable Integer roomId) {
+		roomService.deleteRoom(roomId);
+		return "redirect:/rooms";
+	}
+	
+	@ExceptionHandler({Exception.class})
+	public String handleException(Exception e, Model model) {
+		e.printStackTrace();
+		model.addAttribute("message", e.getMessage());
+		return "error";
+	}
 }
